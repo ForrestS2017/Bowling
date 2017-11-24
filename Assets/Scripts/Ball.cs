2709f6f950666a1dedc028db197b2609e3e64393
Forrest Smith <fsmith2017@gmail.com>
2017-11-24 01:22:53 -0500

    1) Basic controls and UI

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public bool inPlay = false;
+    private Rigidbody rigidBody;
+    private AudioSource audioSource;
+
+    public Vector3 launchVector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+    }
+
+    public void Launch(Vector3 velocity)
+    {
+        inPlay = true;
+        rigidBody.useGravity = true;
+        rigidBody.velocity = velocity;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
